9cbfb53dc858ab53c93c8a729cbaa0493640b090
Joe Sinfield <josephsinfield@yahoo.com>
2020-03-30 16:43:25 +0100

    Include eslint as basic requirement of all projects
    This change is being made before the upcoming project name change. This is no longer a dotfiles specific generator. It generates the project components which are required for all jslog npm projects.

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,6 +14,26 @@ module.exports = class extends MyBase {
   writing() {
     this.copyTemplateFiles();
     this.config.save();
+    const pkgJson = {
+      name: 'basic-jslog-project',
+      version: '0.1.0',
+      description: 'A basic project with only the essentials which all jslog projects require',
+      private: true,
+      author: 'js-jslog <josephsinfield@yahoo.com>',
+      license: 'UNLICENSED',
+      scripts: {
+        test: '"echo "Error: no test specified" && exit 1',
+        postinstall: 'npx eslint --init'
+      },
+      devDependencies: {
+        eslint: '^6.8.0'
+      },
+    };
+
+    this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+  }
+
+  install() {
+    this.npmInstall();
   }
 };
-
